362d04791a851e4f0b2350a6e8c14cd5c69b0973
kirsanov69 <kirsanov118@gmail.com>
2024-10-22 15:21:23 +0400

    Initial commit

---
+++ config/config.py
@@ -0,0 +1,59 @@
+
+from dataclasses import dataclass
+
+from environs import Env
+
+
+    
+
+@dataclass
+class TgBot:
+    token: str            # Токен для доступа к телеграм-боту
+
+
+@dataclass
+class Gpt:
+    token: str            # Токен для доступа к телеграм-боту
+    price_assistant_id: str  # Ассистент для работы с пользователем
+
+
+
+
+@dataclass
+class WebhookURL:
+    url: str              # URL вебхука
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    gpt: Gpt
+    webhook_url: WebhookURL
+    
+
+
+
+path = 'C:\\Users\\user\\1\\tg_bots\\test_llm_bot\\.env'
+
+def load_config(path: str | None = None) -> Config:
+
+    # Создаем экземпляр класса Env
+    env: Env = Env()
+
+    # Добавляем в переменные окружения данные, прочитанные из файла .env 
+    env.read_env()
+
+    # Создаем экземпляр класса Config и наполняем его данными из переменных окружения
+    return  Config(
+        tg_bot=TgBot(
+            token=env('BOT_TOKEN')
+        ),
+        gpt=Gpt(
+            token=env('GPT_TOKEN'),
+            price_assistant_id=env('ASSISTANT_ID'),
+        ),
+        webhook_url=WebhookURL(
+            url=env('WEBHOOK_URL')
+        )
+    )
+
